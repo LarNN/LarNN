65778504ae95ca37a0650f099b3b06b781ece7cf
LarNN <54688130+LarNN@users.noreply.github.com>
2025-07-09 19:27:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} başarıyla giriş yaptı!")
+
+@bot.command()
+async def selam(ctx):
+    await ctx.send("Selam Musamert! 👋")
+
+token = os.getenv("MTM5MjQ5OTU2NzY4NTAxMzU3Ng.GEfvFf.XD8RqiSTYtD1Mp2dTHgEi67DDXqATkAzoQeTE4")
+bot.run(token)
