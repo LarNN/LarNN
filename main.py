63b571fd34245c814b410ef8ad4a40aaa9520e6a
LarNN <54688130+LarNN@users.noreply.github.com>
2025-07-09 20:03:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,7 +18,9 @@ async def selam(ctx):
 
 @bot.command()
 async def atla(ctx):
-    await ctx.send("Atla komutu çalıştı! ⏭️")
+    sonuc = random.uniform(0, 100)  # 0 ile 100 arasında float sayı üretir
+    sonuc_str = f"{sonuc:.2f}%"    # Virgülden sonra 2 basamaklı formatla
+    await ctx.send(f"{sonuc_str} atladınız 🪂")
 
 bot.run(os.getenv("TOKEN"))
 
